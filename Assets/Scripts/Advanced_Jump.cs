a9ee79511f36e519fa791ffe38bc27aef4e0150c
Bashakj49 <56273882+Bashakj49@users.noreply.github.com>
2019-10-28 15:39:53 -0500

    Unity Project
    Placed unity prj into github repo

---
+++ Assets/Scripts/Advanced_Jump.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Advanced_Jump : MonoBehaviour
+{
+    public float fallX = 2.5f;
+    public float lowJumpX = 2f;
+
+    Rigidbody2D rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallX - 1) * Time.deltaTime;
+        } 
+        else if (rb.velocity.y > 0 && !Input.GetButton ("Jump"))
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpX - 1) * Time.deltaTime;
+        }
+    
+
+    }
+}
---
+++ Assets/Scripts/Background_Controller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background_Controller : MonoBehaviour
+{
+
+    private float length, startpos;
+    public GameObject cam;
+    public float parallax;
+
+
+    void Start()
+    {
+        startpos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+   
+    void FixedUpdate()
+    {
+        float dist = (cam.transform.position.x * parallax);
+        transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+    }
+}
